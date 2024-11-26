31cd6793d5c4f4fc81dd1ce3121534d7d56783e7
jackberck <berckjack@gmail.com>
2024-11-26 14:04:11 +0700

    init: project setup

---
+++ tailwind.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+      },
+      colors: {
+        "blue-base": "#1a73e8",
+        "dark-base": "#111827",
+        "light-base": "#f9fafb",
+		"purple-base": "#7c3aed",
+      },
+      fontFamily: {
+        poppins: "Poppins",
+      },
+    },
+  },
+  plugins: [],
+};
